696e18d8664d278384b8ff2770cf5ac25a4312fb
ysa2shi <satoshi.1109.work@gmail.com>
2026-05-24 14:47:04 +0900

    first commit

---
+++ src/main/java/com/example/springbootgrpcsample/SpringbootGrpcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgrpcsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGrpcSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGrpcSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootgrpcsample/SpringbootGrpcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgrpcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGrpcSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c9a43c0ca6bb7ff3a0379db86e4c7ca297dbf7bd
ysa2shi <satoshi.1109.work@gmail.com>
2026-05-24 16:45:12 +0900

    StatusRuntimeExceptionを設定

--- src/main/java/com/example/springbootgrpcsample/grpc/client/HelloGrpcClient.java
+++ src/main/java/com/example/springbootgrpcsample/grpc/client/HelloGrpcClient.java
@@ -3,10 +3,13 @@ package com.example.springbootgrpcsample.grpc.client;
 import com.example.grpcsample.proto.HelloRequest;
 import com.example.grpcsample.proto.HelloResponse;
 import com.example.grpcsample.proto.HelloServiceGrpc;
+import io.grpc.StatusRuntimeException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.client.GrpcChannelFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HelloGrpcClient {
@@ -24,8 +27,12 @@ public class HelloGrpcClient {
                 .setName(name)
                 .build();
 
-        HelloResponse response = stub.sayHello(request);
-
-        return response.getMessage();
+        try {
+            HelloResponse response = stub.sayHello(request);
+            return response.getMessage();
+        } catch (StatusRuntimeException ex) {
+            log.error("gRPC call failed. status={}", ex.getStatus(), ex);
+            throw ex;
+        }
     }
 }
